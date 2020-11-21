97866404605f59aa8ee1fa5cf8f3a241f9813a23
lena346 <lenashashkina2001@gmail.com>
2020-11-22 02:28:34 +0300

    First init

--- src/main/java/com/mycompany/hiber/dao/UserDao.java
+++ src/main/java/com/mycompany/hiber/dao/UserDao.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.hiber.dao;
 
-/**
- *
- * @author lenas
- */
+import com.mycompany.hiber.models.Auto;
+import com.mycompany.hiber.models.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import com.mycompany.hiber.utils.HibernateSessionFactoryUtil;
+import java.util.List;
+
 public class UserDao {
-    
+
+    public User findById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(User.class, id);
+    }
+
+    public void save(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.save(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public void update(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.update(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public void delete(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public Auto findAutoById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Auto.class, id);
+    }
+
+    public List<User> findAll() {
+        List<User> users = (List<User>)  HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery("From User").list();
+        return users;
+    }
 }
---
+++ src/main/java/com/mycompany/hiber/service/UserService.java
@@ -0,0 +1,42 @@
+package com.mycompany.hiber.service;
+
+
+import com.mycompany.hiber.dao.UserDao;
+import com.mycompany.hiber.models.Auto;
+import com.mycompany.hiber.models.User;
+
+import java.util.List;
+
+public class UserService {
+
+    private UserDao usersDao = new UserDao();
+
+    public UserService() {
+    }
+
+    public User findUser(int id) {
+        return usersDao.findById(id);
+    }
+
+    public void saveUser(User user) {
+        usersDao.save(user);
+    }
+
+    public void deleteUser(User user) {
+        usersDao.delete(user);
+    }
+
+    public void updateUser(User user) {
+        usersDao.update(user);
+    }
+
+    public List<User> findAllUsers() {
+        return usersDao.findAll();
+    }
+
+    public Auto findAutoById(int id) {
+        return usersDao.findAutoById(id);
+    }
+
+
+}
